ed7ef799b93bcd8a2ecdb43369f21664660b2b9a
Abel desk <abelprivas@gmail.com>
2024-01-14 15:47:41 -0300

    ver 1.0 js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,30 @@
 precio = 400000
 
-precioSpan = document.querySelector(".precio-inicial");
+precioSpan = document.querySelector(".precio-inicial")
 precioSpan.innerHTML = precio
+
+//totales y cantidades
+total = document.querySelector(".valor-total")
+cant = document.querySelector(".cantidad")
+act = cant.innerHTML
+tot = total.innerHTML
+
+//variable botones
+const btnsum = document.querySelector("#bmax")
+const btnmin = document.querySelector("#bmin")
+
+//suma
+btnsum.addEventListener('click',function(){
+cant.innerHTML = ++act
+total.innerHTML = precio * +act
+})
+
+//resta
+btnmin.addEventListener('click',function(){
+    cant.innerHTML = --act
+    total.innerHTML = precio * +act
+})
+
+
+
+
